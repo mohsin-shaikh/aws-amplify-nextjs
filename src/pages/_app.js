4f6d03b2ca21d572d36970b9ae60efb349c8422f
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2023-09-30 16:10:12 +0530

    Added Aws Amplify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,9 @@
-import '@/styles/globals.css'
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
+// import '@/styles/globals.css'
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
